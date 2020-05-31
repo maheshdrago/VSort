3eb307336b1438ee935b570bbfb360cad3c52fc5
mahesh <mahesh6273766@gmail.com>
2020-06-01 04:36:41 +0530

    Updates

---
+++ precache-manifest.df0f63cb6f7992ee6daf5fc698ed4ef1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e908c7864c6f50410d5b59a483ce0f2",
+    "url": "/VSort/index.html"
+  },
+  {
+    "revision": "4363e4881c84bc1f2323",
+    "url": "/VSort/static/css/main.9ebdc0b0.chunk.css"
+  },
+  {
+    "revision": "ae8e70fb18eb74e9f461",
+    "url": "/VSort/static/js/2.876611bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VSort/static/js/2.876611bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4363e4881c84bc1f2323",
+    "url": "/VSort/static/js/main.5fff3aa8.chunk.js"
+  },
+  {
+    "revision": "26f5346ee36c03ccc053",
+    "url": "/VSort/static/js/runtime-main.286bb723.js"
+  }
+]);
